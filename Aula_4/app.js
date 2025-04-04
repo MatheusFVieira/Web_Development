9787fa2c1c6db86ce4eb0f8d4f92044909cd7d68
MatheusFVieira <matheus.vf06@gmail.com>
2025-04-03 22:42:05 -0300

    Aula_4

---
+++ Aula_4/app.js
@@ -0,0 +1,23 @@
+// alert("Hello World!")
+
+// let nome = prompt("Qual é o seu nome?", "Insira aqui o seu nome")
+// alert(nome)
+
+// let resultado = confirm("Você deseja seguir?")
+
+// console.log(`Olá ${nome}, tudo bem?`)
+// console.log(typeof nome)
+
+
+// let nome = prompt("Qual é o seu nome?", "Insira aqui o seu nome")
+// let idade = prompt("Qual é a sua idade?", "Insira sua idade")
+// let profissao = prompt("Qual é a sua profissão", "Insira a sua profissão")
+
+// console.log(`Seu nome é ${nome}, você tem ${idade} anos e sua profissão é ${profissao}!`)
+
+let num1 = parseInt(prompt("Digite um número"))
+let num2 = parseInt(prompt("Digite outro número"))
+
+let resultado = num1 + num2
+
+alert(resultado)
